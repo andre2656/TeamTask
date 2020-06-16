d8c06cabdd7e98b5a830cb727a876867e9fa0601
Andre <andre2656@gmail.com>
2020-06-16 15:58:26 -0600

    Added structure to  Cards

--- client/src/components/MainApp/ProjectInfo.js
+++ client/src/components/MainApp/ProjectInfo.js
@@ -9,98 +9,124 @@ class ProjectInfo extends Component {
 
     render() {
         return (
-            <div style={{ marginTop: 50, }}>
+            <div style={{ marginTop: 50, display: 'flex', flexDirection: 'column' }}>
 
-                <div className='card'>
-                    <div className='col-md-2'>
-                        <p style={{ fontSize: 22, fontWeight: 'bold', textAlign: 'left', paddingTop: 10, height: 36 }}>The Beginning</p>
+                
+                    <div className='col-md-12'>
+                        <p style={{ fontSize: 22, fontWeight: 'bold', textAlign: 'left', padding: '8 4 4 8', height: 36, display: 'block', width: '100%' }}>The Beginning</p>
                     </div>
-                </div>
-                <div style={{width: '100%', display: 'flex'}}>
-                    <div className='card' id='Card'>
-                        <div className='col-md-12'>
-                            <p style={{ fontSize: 22, marginTop: 10, }}>Personnel</p>
-                        </div>
-                        <div className='row' id='Presonnel'>
-                            <p id='Name'>Terrence Campbell </p>
-                            <p id='Position'> Creative Direction</p>
-                        </div>
-                        <div className='row' id='Presonnel'>
-                            <p id='Name'>Helen Aguilar </p>
-                            <p id='Position'>Costume/Prop</p>
-                        </div>
-                        <div className='row' id='Presonnel'>
-                            <p id='Name'>Nasim Campbell</p>
-                            <p id='Position'>Styling</p>
-                        </div>
-                        <div className='row' id='Presonnel'>
-                            <p id='Name'>Kwe Johnson</p>
-                            <p id='Position'>Photographer</p>
-                        </div>
-                        <div className='row' id='Presonnel'>
-                            <p id='Name'>TBD</p>
-                            <p id='Position'> Hair/Make Up</p>
-                        </div>
-                        <div className='row' id='Presonnel'>
-                            <p id='Name'>TBD</p>
-                            <p id='Position'>CPA</p>
-                        </div>
-                    </div>
-                    <div className='card' id='Card'>
-                        <div className='col-md-12'>
-                            <p style={{ fontSize: 16, textAlign: 'center' }}>Location</p>
-                        </div>
-                        <div className='Location'>
-                            <p style={{ fontSize: 16, fontWeight: 'bold', textAlign: 'left' }}>Broadacres Elementary</p>
-                        </div>
-                        <div className='Location'>
-                            <p style={{ fontSize: 16, fontWeight: 'bold', textAlign: 'left' }}>Leapwood Elementary</p>
-                        </div>
-                        <div className='Location'>
-                            <p style={{ fontSize: 16, fontWeight: 'bold', textAlign: 'left' }}>Courtis JH <br /> <br /> Add in Links to the locations and googleMaps photos</p>
+                <div style={{width: '100%', }} id='Board'>
+              
+                    <div id='Wrapper'>
+                        <div className='card' id='Card'>
+                            <div className='col-md-12'>
+                                <p id='cardHeader'>Personnel</p>
+                            </div>
+                            <div className='row' id='Presonnel'>
+                                <p id='Name'>Terrence Campbell </p>
+                                <p id='Position'> Creative Direction</p>
+                            </div>
+                            <div className='row' id='Presonnel'>
+                                <p id='Name'>Helen Aguilar </p>
+                                <p id='Position'>Costume/Prop</p>
+                            </div>
+                            <div className='row' id='Presonnel'>
+                                <p id='Name'>Nasim Campbell</p>
+                                <p id='Position'>Styling</p>
+                            </div>
+                            <div className='row' id='Presonnel'>
+                                <p id='Name'>Kwe Johnson</p>
+                                <p id='Position'>Photographer</p>
+                            </div>
+                            <div className='row' id='Presonnel'>
+                                <p id='Name'>TBD</p>
+                                <p id='Position'> Hair/Make Up</p>
+                            </div>
+                            <div className='row' id='Presonnel'>
+                                <p id='Name'>TBD</p>
+                                <p id='Position'>CPA</p>
+                            </div>
                         </div>
                     </div>
-                    <div className='card' id='Card'>
-                        <div className='col-md-12'>
-                            <p style={{ fontSize: 16, textAlign: 'center' }}>Details</p>
-                        </div>
-                        <div className='Location'>
-                            <p style={{ fontSize: 16, fontWeight: 'bold', textAlign: 'left' }}>Blah</p>
-                        </div>
-                    </div>
-                    <div className='card' id='Card'>
-                        <div className='col-md-12'>
-                            <p style={{ fontSize: 16, textAlign: 'center' }}>Cast</p>
-                        </div>
-                        <div className='Location'>
-                            <p style={{ fontSize: 16, fontWeight: 'bold', textAlign: 'left' }}>Blah</p>
-                        </div>
-                    </div>
-                    <div className='card' id='Card'>
-                        <div className='col-md-12'>
-                            <p style={{ fontSize: 16, textAlign: 'center' }}>Budget</p>
-                        </div>
-                        <div className='Location'>
-                            <p style={{ fontSize: 16, fontWeight: 'bold', textAlign: 'left' }}>Blah</p>
+                    <div id='Wrapper'>
+                        <div className='card' id='Card'>
+                            <div className='col-md-12'>
+                                <p id='cardHeader'>Location</p>
+                            </div>
+                            <div id='list-cards'>
+                                <div id='cardContent'>
+                                    <p id= 'cardText'>Broadacres Elementary</p>
+                                </div>
+                                <div id='cardContent'>
+                                    <p id= 'cardText'>Leapwood Elementary</p>
+                                </div>
+                                <div id='cardContent'>
+                                    <p id= 'cardText'>Courtis JH <br /> <br /> Add in Links to the locations and googleMaps photos</p>
+                                </div>
+                            </div>
+                           
                         </div>
                     </div>
-                    <div className='card' id='Card'>
-                        <div className='col-md-12'>
-                            <p style={{ fontSize: 16, textAlign: 'center' }}>Shoot Dates</p>
-                        </div>
-                        <div className='Location'>
-                            <p style={{ fontSize: 16, fontWeight: 'bold', textAlign: 'left' }}>Broadacres Elementary</p>
-                        </div>
-                        <div className='Location'>
-                            <p style={{ fontSize: 16, fontWeight: 'bold', textAlign: 'left' }}>Leapwood Elementary</p>
-                        </div>
-                        <div className='Location'>
-                            <p style={{ fontSize: 16, fontWeight: 'bold', textAlign: 'left' }}>Courtis JH <br /> <br /> Add in Links to the locations and googleMaps photos</p>
+                    <div id='Wrapper'>
+                        <div className='card' id='Card'>
+                            <div className='col-md-12'>
+                                <p id='cardHeader'>Details</p>
+                            </div>
+                            <div id='list-cards'>
+                                <div id='cardContent'>
+                                    <p id='cardText'>Blah</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className='card' id='Card'>
-                        <div className='col-md-12'>
-                            <p style={{ fontSize: 16, textAlign: 'center' }}>Notes</p>
+                    <div id='Wrapper'>
+                        <div className='card' id='Card'>
+                            <div className='col-md-12'>
+                                <p id='cardHeader'>Cast</p>
+                            </div>
+                            <div id='list-cards'>
+                                <div id='cardContent'>
+                                    <p id='cardText'>Blah</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id='Wrapper'>
+                        <div className='card' id='Card'>
+                            <div className='col-md-12'>
+                                <p>Budget</p>
+                            </div>
+                            <div id='list-cards'>
+                                <div id='cardContent'>
+                                    <p id='cardText'>Blah</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id='Wrapper'>
+                        <div className='card' id='Card'>
+                            <div className='col-md-12'>
+                                <p id='cardHeader'>Shoot Dates</p>
+                            </div>
+                            <div id='list-cards'>
+                                <div id='cardContent'>
+                                    <p id='cardText'>Broadacres Elementary</p>
+                                </div>
+                                <div id='cardContent'>
+                                    <p id='cardText'>Leapwood Elementary</p>
+                                </div>
+                                <div id='cardContent'>
+                                    <p id='cardText'>Courtis JH <br /> <br /> Add in Links to the locations and googleMaps photos</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id='Wrapper'>
+                        <div className='card' id='Card'>
+                            <div className='col-md-12'>
+                                <p id='cardHeader'>Notes</p>
+                            </div>
+                            <div id='list-cards'></div>
                         </div>
                     </div>
                 </div>
