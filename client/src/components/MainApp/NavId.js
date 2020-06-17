750ae35433078bcae44dd29d51538a56a9c613e3
Andre <andre2656@gmail.com>
2020-06-16 21:58:56 -0600

    Updated Nav

--- client/src/components/MainApp/NavId.js
+++ client/src/components/MainApp/NavId.js
@@ -20,9 +20,10 @@ class NavId extends Component {
         loginController.removeUserChangedListener(this.setUser);
     }
     setUser = (user) => {
-        this.setState({ user: user });
-        console.log('helllloooo')
-        this.UsernameInitials(user)
+        this.setState({ user: user }, () => {
+            this.UsernameInitials(user)
+        });
+        
     }
     UsernameInitials = (user) => {
         console.log(user+ ' hello  sdf' + user.firstName) 
@@ -35,18 +36,19 @@ class NavId extends Component {
             <div>
                 <div id="navBar" >
                     <div className='col-md-4'/>
-                    <div className='col-md-4' id="title" style={{textAlign: "center"}}><h2>Team Task</h2></div>
+                    <div className='col-md-4' style={{ textAlign: "center" }}><button id='title'>Team Task</button></div>
+                    
                     <div className='col-md-1'/>
                     <div className='col-md-1' />
                     <div className='col-md-1' >
 
                     </div>
                     <div id='UserDropdown' className='col-md-1' style={{  }}>
-                        <button type="button" id="UserDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onClick={this.UserDrop}>
+                        <button type="button" id="UserDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id='expandProfile' onClick={this.UserDrop}>
                             AC
                         </button>
                         <div className="dropdown-menu" aria-labelledby="dropdownMenuButton" id='dropdownMenu'>
-                            <Link className="sign-out" to="/"><button type="button" className="btn btn-light" id="signout" onClick={this.logout} value="Log out">Logout</button></Link>
+                            <Link className="sign-out" to="/" id="signout"><button onClick={this.logout} value="Log out">Logout</button></Link>
                         </div>
                     </div>
                 </div>
